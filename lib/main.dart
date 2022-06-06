aef23b3568415ec7e83a0709074296db0edd7b5f
yosiruma <yosiruma@gmail.com>
2022-06-07 01:34:22 +0900

    タイルにお気に入りアイコンを追加

--- lib/main.dart
+++ lib/main.dart
@@ -51,10 +51,16 @@ class _RandomWordsState extends State<RandomWords> {
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
+        final alreadySaved = _saved.contains(_suggestions[index]);
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
+          ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
           ),
         );
       },
